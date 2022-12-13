b448c8d507f9ae435e2c4eb3873118b63c5116c5
452Harsh <harshtuli452@gmail.com>
2022-12-13 17:47:51 +0530

    harsh

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/",function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=9cfa202c13a5bfb3f62be337acc9d8fe&units=metric#";
-    https.get(url,function(response){
-        response.on("data",function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const temp1 = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            console.log(temp1);
-            res.write("<p> The weather is currently " + temp1 +  "</p>");
-            res.write("<h1>The temperature in paris is "+temp + " degree celcius</h1>");
-            res.write("<img src=" + imageurl + ">");
-            res.send()
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/",function(req,res){
+        const query = req.body.cityName;
+        const apikey = "9cfa202c13a5bfb3f62be337acc9d8fe";
+        const units = "metric#";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + units;
+        https.get(url,function(response){
+            response.on("data",function(data){
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.main.temp;
+                const temp1 = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+                console.log(temp1);
+                res.write("<p> The weather is currently " + temp1 +  "</p>");
+                res.write("<h1>The temperature in "+query+" is "+temp + " degree celcius</h1>");
+                res.write("<img src=" + imageurl + ">");
+                res.send()
+            })
         })
-    })
 })
+
+
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
 })
